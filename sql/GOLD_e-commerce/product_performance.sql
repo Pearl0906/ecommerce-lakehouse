ad73b7c56bd7f8c73dc11b8bcd30f87f4b97ea39
Pearl0906 <shungubepearlzinhle@gmail.com>
2026-07-07 16:52:13 +0000

    Changed "unit" to "item"

--- sql/GOLD_e-commerce/product_performance.sql
+++ sql/GOLD_e-commerce/product_performance.sql
@@ -4,7 +4,7 @@ SELECT
     oi.product_id,
     p.product_name,
     SUM(oi.quantity) AS units_sold,
-    SUM(oi.quantity * oi.unit_price) AS revenue
+    SUM(oi.quantity * oi.item_price) AS revenue
 FROM silver_order_items oi
 JOIN silver_products p
 ON oi.product_id = p.product_id
--- sql/GOLD_e-commerce/sales_by_category.sql
+++ sql/GOLD_e-commerce/sales_by_category.sql
@@ -2,7 +2,7 @@ CREATE OR REFRESH MATERIALIZED VIEW gold_sales_by_category AS
 
 SELECT
     p.category,
-    SUM(oi.quantity * oi.unit_price) AS revenue,
+    SUM(oi.quantity * oi.item_price) AS revenue,
     SUM(oi.quantity) AS items_sold
 FROM silver_order_items oi
 JOIN silver_products p
