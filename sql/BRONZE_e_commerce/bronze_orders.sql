d399cd978f26bc99dc6ec51047dd77ecffced774
Pearl0906 <shungubepearlzinhle@gmail.com>
2026-07-07 15:43:28 +0000

    dropped the table to create a new table

--- sql/BRONZE_e_commerce/bronze_orders.sql
+++ sql/BRONZE_e_commerce/bronze_orders.sql
@@ -1,4 +1,6 @@
-CREATE OR REFRESH STREAMING TABLE bronze_orders
+DROP TABLE IF EXISTS workspace.default.bronze_orders;
+
+CREATE STREAMING TABLE bronze_orders
 AS SELECT *,
   current_timestamp() AS _ingestion_timestamp,
   _metadata.file_name AS _source_file
